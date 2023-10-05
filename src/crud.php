32eb09516912372c624a9dd6ad6a1ea63d577f48
tekllin <nathaniel.favario@gmail.com>
2023-10-05 18:52:55 +0200

    cour crud

---
+++ src/crud.php
@@ -0,0 +1,28 @@
+<?php
+require_once "./src/dbConnect.php";
+
+
+//fonction getById
+$statement = $connection->query("SELECT * FROM contact WHERE `name` = 'Favario' AND `surname` = '".htmlspecialchars($_GET['surname']). " '");
+$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+//fonction create
+$statement = $connection->prepare("INSERT INTO `contact` (`name`, `surname`, `status`)  VALUES ('Favario', 'Nathaniel', 'online') ");
+$statement->bindParam(1,$_GET['surname']);
+$statement->bindParam(2,$_GET['name']);
+$statement->execute();
+
+//fonction getAll
+$statement = $connection->query("SELECT * FROM contact WHERE 1");
+$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+//fonction delete
+$statement = $connection->prepare("DELETE FROM `contact` WHERE id=?");
+$id = 3;
+$statement->bindParam(1,$id);
+$statement->execut();
+
+//fonction update
+$statement = $connexion->prepare("UPDATE `contact` SET `status` = offline WHERE id = ?");
+$id = 2;
+$statement->bindParam(1, $id);
+$statement->execute();
--- src/dbConnect.php
+++ src/dbConnect.php
@@ -2,7 +2,3 @@
 
 $connection = new PDO('mysql:host=' . $globalConfigs["database"]["host"] . ';port=' . $globalConfigs['database']["port"] . ';dbname=' . $globalConfigs['database']["db_name"] . '', $globalConfigs['database']['user'], $globalConfigs['database']['password']);
         
-$statement = $connection->prepare("INSERT INTO `contact` (`name`, `surname`, `status`)  VALUES ('Favario', 'Nathaniel', 'online') ");
-// $stmt->bindParam(1,$id);
-$statement->execute();
-    // 
